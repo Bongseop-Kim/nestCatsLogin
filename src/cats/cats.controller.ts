0c30229352dca3a5acc74e20f3fa77cc74d5fd11
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-20 00:38:11 +0900

    불필요한 데이터 제거

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,13 +3,11 @@ import {
   Controller,
   Get,
   Post,
-  Req,
   UseFilters,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { Request } from 'express';
 import { AuthService } from 'src/auth/auth.service';
 import { LoginRequestDto } from 'src/auth/dto/login.request.dto';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
