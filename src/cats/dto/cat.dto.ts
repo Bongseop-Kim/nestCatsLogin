088704950f21843ebf4a016b0625e26c16f30361
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-23 18:56:54 +0900

    주석

--- src/cats/dto/cat.dto.ts
+++ src/cats/dto/cat.dto.ts
@@ -2,6 +2,8 @@ import { ApiProperty, PickType } from '@nestjs/swagger';
 import { Cat } from '../cats.schema';
 
 export class ReadOnlyCatDto extends PickType(Cat, ['email', 'name'] as const) {
+  //dto에서 interface를 사용하지 않고 class를 사용하는 이유는 자바스크립트 단에서도 타입 검증이 가능하다.
+  //interface는 typescript 문법으로 js로 변경되면 사라지기 때문이다.
   @ApiProperty({
     example: '1092012890',
     description: 'id',
